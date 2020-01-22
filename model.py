b88c38f93e5d087d6d86348493eed80d3ce3fa7d
AlexSananka <sananka43@gmail.com>
2020-01-22 22:51:46 +0000

    modify model

--- model.py
+++ model.py
@@ -32,15 +32,16 @@ class CNN_1(nn.Module):
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)
         return x
+
 class CNN_2(nn.Module):
     def __init__(self, input_size, n_feature, output_size):
         super(CNN_2, self).__init__()
         self.n_feature = n_feature
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_feature, kernel_size=5)
-        self.conv2 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
-        self.conv3 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
-        self.conv4 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
-        self.fc1 = nn.Linear(n_feature*36*36, 50)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=5)
+        self.fc1 = nn.Linear(128*10*10, 50)
         self.fc2 = nn.Linear(50, 2)
        
     def forward(self, x, verbose=False):
@@ -56,7 +57,7 @@ class CNN_2(nn.Module):
         x = self.conv4(x)
         x = F.relu(x)
         x = F.max_pool2d(x, kernel_size=2)       
-        x = x.view(-1, self.n_feature*36*36)
+        x = x.view(-1, 128*10*10)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
